61e6e2f07d6a9e943408829c59822bb92ac60116
Richard Lee <r7568436@gmail.com>
2023-09-08 17:38:37 +0800

    9/8

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "ACtion", DisplayOreder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOreder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOreder = 3 }
+                );
+        }
+    }
+}
