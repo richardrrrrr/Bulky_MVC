a0fcd77488edf11eeda95005f9baefa1b876bb36
Richard Lee <r7568436@gmail.com>
2023-09-18 15:57:54 +0800

    9/18

---
+++ Bulky.DataAcess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAcess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category obj);
+	
+	}
+}
